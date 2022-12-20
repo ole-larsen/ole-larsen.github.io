25b15c7ef797029457d5d878bfc729eb18aac64f
Oleg Nazarov <ole.larssen777@gmail.com>
2022-12-21 02:04:40 +0530

    init

---
+++ default.13536cdf.js
@@ -0,0 +1,1 @@
+import{a as t,b as o,r as n,o as s}from"./entry.b8a932c9.js";const c={},r={class:"container content"};function a(e,_){return s(),o("div",r,[n(e.$slots,"default")])}const d=t(c,[["render",a]]);export{d as default};
